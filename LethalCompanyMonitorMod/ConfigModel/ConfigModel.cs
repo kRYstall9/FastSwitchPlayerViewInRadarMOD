a88b8dcc52e33bc07078e8954f6df19d53debd99
Antonio Galiero <antonio.galiero@bcsoft.net>
2023-12-07 17:48:18 +0100

    Added a generic model for configs

---
+++ LethalCompanyMonitorMod/ConfigModel/ConfigModel.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LethalCompanyMonitorMod.ConfigModel
+{
+    public class ConfigModel<T>
+    {
+        public T DefaultValue { get; set; }
+        public string Description { get; set; }
+        public string Key { get; set; }
+        public string Section { get; set; }
+        public ConfigEntry<T> ConfigEntry { get; set; }
+
+        public ConfigModel(T defaultValue, string description, string key, string section) 
+        {
+            DefaultValue = defaultValue;
+            Description = description;
+            Key = key;
+            Section = section;
+        }
+    }
+}
---
+++ LethalCompanyMonitorMod/Configs.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx.Configuration;
+using LethalCompanyMonitorMod.ConfigModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LethalCompanyMonitorMod
+{
+    public class Configs
+    {
+        public static readonly ConfigModel<Key> PreviousPlayerCam = new ConfigModel<Key>(UnityEngine.InputSystem.Key.LeftArrow, "Get previous player's cam", "PrevPCam", "HotKeys");
+        public static readonly ConfigModel<Key> NextPlayerCam = new ConfigModel<Key>(UnityEngine.InputSystem.Key.RightArrow, "Get next player's cam", "NextPCam", "HotKeys");
+        public static readonly ConfigModel<Color> AfternoonClockColor = new ConfigModel<Color>(new Color(1f, 0.65f, 0f), "Changes the terminal clock color when it is afternoon. Default Color: Orange", "Afternoon", "ClockColors");
+        public static readonly ConfigModel<Color> EveningClockColor = new ConfigModel<Color>(new Color(0.68f, 0f, 0f), "Changes the terminal clock color when it gets late. Default Color: Red", "Evening", "ClockColors");
+        public static readonly ConfigModel<Color> MorningClockColor = new ConfigModel<Color>(new Color(0f, 1f, 0f), "Changes the terminal clock color at game start. Default Color: Green", "Morning", "ClockColors");
+        public static readonly ConfigModel<Color> NightClockColor = new ConfigModel<Color>(new Color(0.68f, 0f, 0f), "Changes the terminal clock color when it gets night. Default Color: Red", "Night", "ClockColors");
+
+
+
+
+        public static readonly ConfigModel<Key>[] AllBinds = {PreviousPlayerCam, NextPlayerCam};
+        public static readonly ConfigModel<Color>[] TerminalClockColors = { AfternoonClockColor, EveningClockColor, MorningClockColor, NightClockColor};
+
+
+    }
+}
