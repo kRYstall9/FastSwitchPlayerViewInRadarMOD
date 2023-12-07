e762e94ce4fe2b8728e4402058b551d642fb53bf
Antonio Galiero <antonio.galiero@bcsoft.net>
2023-12-07 17:56:52 +0100

    Solved a bug where being alone in the server trying to swap the radar map resulted in some ArgumentOutOfRangeException. Added a clock on the top-right corner of the terminal

--- LethalCompanyMonitorMod/Patch/TerminalPatch.cs
+++ LethalCompanyMonitorMod/Patch/TerminalPatch.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 using HarmonyLib;
+using TMPro;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace LethalCompanyMonitorMod.Patch
@@ -9,6 +11,37 @@ namespace LethalCompanyMonitorMod.Patch
     [HarmonyPatch(typeof(Terminal))]
     public class TerminalPatch
     {
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void CreateClockObjectInstance(ref Terminal __instance)
+        {
+            Transform terminalContainer = ((Component)__instance).transform.parent.parent.Find("Canvas").Find("MainContainer");
+            try
+            {
+                if(Plugin.ClockObject == null)
+                {
+                    Plugin.Log.LogInfo("In the catch ");
+                    Plugin.ClockObject = UnityEngine.Object.Instantiate<GameObject>(((Component)terminalContainer.Find("CurrentCreditsNum")).gameObject, terminalContainer);
+                    ((UnityEngine.Object)Plugin.ClockObject).name = "ClockContainer";
+                    Plugin.ClockObject.transform.localPosition = new Vector3(240f, 205f, -1f);
+                    Plugin.ClockObject.transform.localScale = new Vector3(0.6f, 0.6f, 1f);
+                    Plugin.TerminalClockText = Plugin.ClockObject.GetComponent<TextMeshProUGUI>();
+                    ((TMP_Text)Plugin.TerminalClockText).text = " ";
+                    return;
+                }
+                Plugin.ClockObject = ((Component)terminalContainer.Find("ClockContainer")).gameObject;
+                Plugin.TerminalClockText = Plugin.ClockObject.GetComponent<TextMeshProUGUI>();
+            }
+            catch
+            {
+                Plugin.Log.LogError("Error while creating the clock object instance");
+                Plugin.ClockObject = null;
+                Plugin.TerminalClockText = null;
+            }
+        }
+
+
         [HarmonyPostfix]
         [HarmonyPatch("ParsePlayerSentence")]
         private static void HandleSentence(ref Terminal __instance)
@@ -39,7 +72,7 @@ namespace LethalCompanyMonitorMod.Patch
 
             if (Plugin.ViewMonitorSubmitted)
             {
-                var keyPressed = Array.Find(CustomActions.AllBinds, k => Keyboard.current[k.ConfigEntry.Value].wasPressedThisFrame);
+                var keyPressed = Array.Find(Configs.AllBinds, k => Keyboard.current[k.ConfigEntry.Value].wasPressedThisFrame);
 
                 if (keyPressed != null || keyPressed != default)
                 {
@@ -47,24 +80,31 @@ namespace LethalCompanyMonitorMod.Patch
                     int currentlyViewingPlayer = Plugin.CurrentlyViewingPlayer;
                     int maxSpectablePlayers = Plugin.SelectableObjects.Count;
                     int currentIndex = Plugin.SelectableObjects.IndexOf(currentlyViewingPlayer);
-
-                    switch (keyPressed.Hotkey)
+                    
+                    if(maxSpectablePlayers <= 1)
                     {
-
-                        case Key.LeftArrow:
-                            Plugin.CurrentlyViewingPlayer = currentIndex > 0 ? Plugin.SelectableObjects[currentIndex - 1] : Plugin.SelectableObjects.Last();
-                            break;
-                        case Key.RightArrow:
-                            Plugin.CurrentlyViewingPlayer = currentIndex < (maxSpectablePlayers - 1) ? Plugin.SelectableObjects[currentIndex + 1] : Plugin.SelectableObjects.First();                    
-                            break;
-                        default:
-                            break;
-
+                        Plugin.CurrentlyViewingPlayer = currentlyViewingPlayer;
                     }
+                    else
+                    {
+                        switch (keyPressed.DefaultValue)
+                        {
+
+                            case Key.LeftArrow:
+                                Plugin.CurrentlyViewingPlayer = currentIndex > 0 ? Plugin.SelectableObjects[currentIndex - 1] : Plugin.SelectableObjects.Last();
+                                break;
+                            case Key.RightArrow:
+                                Plugin.CurrentlyViewingPlayer = currentIndex < (maxSpectablePlayers - 1) ? Plugin.SelectableObjects[currentIndex + 1] : Plugin.SelectableObjects.First();
+                                break;
+                            default:
+                                break;
+
+                        }
+                    }
+                    
                     UnityEngine.Object.FindObjectOfType<StartOfRound>().mapScreen.SwitchRadarTargetAndSync(Plugin.CurrentlyViewingPlayer);
                 }
             }
         }
-
     }
 }
