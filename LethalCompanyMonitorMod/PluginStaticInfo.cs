e6275761e8b3f720ddb58e9c245e02298584de5b
kRYstall9 <antonio99044@hotmail.it>
2025-02-07 01:32:03 +0100

    Updated plugin version

--- LethalCompanyMonitorMod/PluginStaticInfo.cs
+++ LethalCompanyMonitorMod/PluginStaticInfo.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LethalCompanyMonitorMod
+﻿namespace LethalCompanyMonitorMod
 {
     public static class PluginStaticInfo
     {
         public const string PLUGIN_GUID = "krystall9.FastSwitchPlayerViewInRadar";
         public const string PLUGIN_NAME = "FastSwitchPlayerViewInRadar";
-        public const string PLUGIN_VERSION = "1.3.1";
+        public const string PLUGIN_VERSION = "1.3.3";
     }
 }
